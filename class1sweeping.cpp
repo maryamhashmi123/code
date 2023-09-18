7a74031980e42bad00e632e4cd5f4339c7610504
maryamhashmi123 <145352965+maryamhashmi123@users.noreply.github.com>
2023-09-18 17:43:07 +0500

    Add files via upload

---
+++ class1sweeping.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"simple calculator\n";
+    
+    int a= 10;
+    int b=20;
+    int c=a+b;//30
+     a=c-a;
+     b=c-b;
+    cout<<"a="<<a<<"\n";
+        cout<<"b="<<b<<"\n";
+    // cout<<"b="<<b;
+    // int a=c-b;//10
+    // int b=b-a;//10-20
+    
+    
+    // int sum= a+b;
+    // int sub=a-b;
+    // int multiply=a*b;
+    // int divide=a/b;
+    // cout<<"int a="<<a<<"\n";
+    // cout<<"int b="<<b<<"\n\n";
+    
+    // cout<<"sum="<<a<<"+"<<b<<"="<<sum<<"\n";
+    // cout<<"sub="<<a<<"-"<<b<<"="<<sub<<"\n";
+    // cout<<"multiply="<<a<<"*"<<b<<"="<<multiply<<"\n";
+    // cout<<"divide="<<a<<"/"<<b<<"="<<divide<<"\n";
+}
